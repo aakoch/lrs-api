fb111f8f0e6d21ee8535e03bff495f09e55391b8
Adam Koch <adam@dogtired.org>
2017-05-20 15:09:02 -0500

    make Agent return IFI

--- src/main/java/com/adamkoch/lrs/api/Agent.java
+++ src/main/java/com/adamkoch/lrs/api/Agent.java
@@ -7,6 +7,10 @@ import java.util.Optional;
  *
  * <p>"An Agent (an individual) is a persona or system."</p>
  *
+ * <p>An Agent MUST be identified by one (1) of the four types of Inverse Functional Identifiers (see 4.1.2.3 Inverse Functional Identifier);
+ An Agent MUST NOT include more than one (1) Inverse Functional Identifier;
+ An Agent SHOULD NOT use Inverse Functional Identifiers that are also used as a Group identifier.</p>
+ *
  * <p>Created by aakoch on 2017-03-27.</p>
  *
  * @author aakoch
@@ -18,4 +22,6 @@ public interface Agent extends Actor {
     default Optional<ObjectType> getObjectType() {
         return Optional.of(ObjectType.AGENT);
     }
+
+    InverseFunctionalIdentifier getId();
 }
